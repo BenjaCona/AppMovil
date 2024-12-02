2c6bfaba925bfbe9d7308dc778bc1599d57fdc23
BenjaCona <be.cona@duocuc.cl>
2024-12-02 11:37:06 -0300

    Revert "Cambios Finales"
    This reverts commit ffaa304de871786dd630bab7b6ea2799ba92b011.

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,19 +1,9 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.Recetin.Recetin',
-  appName: 'Recetin',
-  webDir: 'www',
-  server: {
-    androidScheme: 'https',
-    cleartext: false, 
-  },
-  plugins: {
-    SplashScreen: {
-      launchShowDuration: 3000,
-      launchAutoHide: true,
-    },
-  },
+  appId: 'io.ionic.starter',
+  appName: 'proyecto005_movil',
+  webDir: 'www'
 };
 
 export default config;
